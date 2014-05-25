bd69ba19020f35914897e2c778c82f3e5d46e27c
jphong <jphofasb@infraware.co.kr>
2014-05-25 18:14:13 +0900

    [20140525][asb] Add constructor argument Context

--- src/com/asb/simplertc/SimpleRTC.java
+++ src/com/asb/simplertc/SimpleRTC.java
@@ -1,11 +1,13 @@
 package com.asb.simplertc;
 
+import android.content.Context;
+
 import com.asb.simplertc.session.Session;
 import com.asb.simplertc.session.SessionConfig;
 
 public class SimpleRTC {
 	
-	public static Session createSession(String name, String instanceId, SessionConfig sessionConfig) {
-		return new Session(name, instanceId, sessionConfig);
+	public static Session createSession(Context contxt, String name, String instanceId, SessionConfig sessionConfig) {
+		return new Session(contxt, name, instanceId, sessionConfig);
 	}
 }
