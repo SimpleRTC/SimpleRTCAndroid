3f088a1e0b79da092900f5d9624a0be9ec8e8ac9
jphong <jphofasb@infraware.co.kr>
2014-05-25 14:41:56 +0900

    [20140525][asb] Add WebRTC module

--- src/com/asb/simplertc/Constants.java
+++ src/com/asb/simplertc/Constants.java
@@ -7,10 +7,10 @@ public class Constants {
 		
 		public static enum SIG_ACTION {
 			DONE(10),
-			CALL(10),
-			RETURN(10),
-			RCALL(10),
-			ACK(10);
+			CALL(11),
+			RETURN(12),
+			RCALL(13),
+			ACK(14);
 			
 			private int value;
 			
@@ -25,18 +25,61 @@ public class Constants {
 		
 		public static enum SIG_TYPE {
 			NONE(100),
-			GUM(100),
-			REQUESTCALL(100),
-			CONNECT(100),
-			DISCONNECT(100),
-			CLASSSTART(100),
-			CLASSEND(100),
-			FINISHCALL(100),
-			REJECTCALL(100);
+			GUM(101),
+			REQUESTCALL(102),
+			CONNECT(103),
+			DISCONNECT(104),
+			CLASSSTART(105),
+			CLASSEND(106),
+			FINISHCALL(107),
+			REJECTCALL(108);
 			
 			private int value;
 			
 			private SIG_TYPE(int value) {
+				this.value = value;
+			}
+			
+			public int value() {
+				return this.value;
+			}
+		};
+	}
+	
+	public static class WEBRTC {
+		public static final String SERVER = "";
+		
+		public static final int CALLER = 10;
+		public static final int CALLEE = 11;
+		
+		public static enum ROLE {
+			NONE(10),
+			CALLER(11),
+			CALLEE(12);
+			
+			private int value;
+			
+			private ROLE(int value) {
+				this.value = value;
+			}
+			
+			public int value() {
+				return this.value;
+			}
+		};
+		
+		public static enum STEP {
+			NONE(100),
+			INIT(101),
+			GUM_SUCCESS(102),
+			GUM_FAILED(103),
+			SIGNALING(104),
+			CONNECTED(105),
+			DISCONNECTED(106);
+			
+			private int value;
+			
+			private STEP(int value) {
 				this.value = value;
 			}
 			
